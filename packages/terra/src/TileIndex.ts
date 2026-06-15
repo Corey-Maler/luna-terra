604a244b2a03462e8ca1f323b2a5c5ea377d4272
Konstantin Zarvanskiy <corey.maler@outlook.com>
2026-06-15 10:23:46 +0200

    feat(terra): extend global tile depth

--- packages/terra/src/TileIndex.ts
+++ packages/terra/src/TileIndex.ts
@@ -1,7 +1,7 @@
 import { V2 } from '@lunaterra/math';
 
 export const TREE_BITS = 16;
-export const MAX_DEPTH = 14;
+export const MAX_DEPTH = 15;
 export type TileIndex = number;
 export type TileIndexString = string;
 
